1894aa99476024cc19f56d62999806b606f14cde
ibleedfilm <rem@lansangan.com>
2018-06-10 16:41:30 +0800

    Select starting speed on popup :sparkles:

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,5 +1,25 @@
-const testDiv = document.createElement('div');
+window.onload = function() {
+  const form = document.getElementsByClassName('playback')[0];
 
-testDiv.innerHTML = 'Hello Popup!';
+  form.addEventListener('submit', event => event.preventDefault());
 
-document.querySelector('body').appendChild(testDiv);
+  const speedOptions = document.getElementsByName('speed')[0];
+
+  const defaultSpeed = 1.5;
+
+  // set initial speed
+  setSpeed(speedOptions, defaultSpeed);
+};
+
+function setSpeed(speedOptions, speed) {
+  const speedValues = Object.values(speedOptions).map(option =>
+    Number.parseFloat(option.value),
+  );
+
+  let selectedIndex = speedValues.indexOf(speed);
+  if (selectedIndex < 0) {
+    selectedIndex = 3;
+  }
+
+  speedOptions.selectedIndex = selectedIndex;
+}
