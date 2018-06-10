939a06c259719671bc700bf713f9edd0bd0bc65f
ibleedfilm <rem@lansangan.com>
2018-06-10 17:12:27 +0800

    Add popup selector event listener :sparkles:

--- src/popup/index.js
+++ src/popup/index.js
@@ -11,6 +11,12 @@ function setSpeed(speedOptions, speed) {
   speedOptions.selectedIndex = selectedIndex; // eslint-disable-line no-param-reassign
 }
 
+function onChangeListener(event, speedOptions) {
+  event.preventDefault();
+
+  console.log(event.target.value);
+}
+
 window.onload = function onLoad() {
   const form = document.getElementsByClassName('playback')[0];
 
@@ -18,8 +24,12 @@ window.onload = function onLoad() {
 
   const speedOptions = document.getElementsByName('speed')[0];
 
-  const defaultSpeed = 1.5;
+  const defaultSpeed = 1.0;
 
   // set initial speed
   setSpeed(speedOptions, defaultSpeed);
+
+  speedOptions.addEventListener('change', event =>
+    onChangeListener(event, speedOptions),
+  );
 };
