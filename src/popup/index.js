6628fa053be9fbaa198989472beec292cb6c7a85
ibleedfilm <rem@lansangan.com>
2018-06-10 16:56:42 +0800

    Fix lint errors :rotating_light:

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,4 +1,17 @@
-window.onload = function() {
+function setSpeed(speedOptions, speed) {
+  const speedValues = Object.values(speedOptions).map(option =>
+    Number.parseFloat(option.value),
+  );
+
+  let selectedIndex = speedValues.indexOf(speed);
+  if (selectedIndex < 0) {
+    selectedIndex = 3; // index of normal speed: 3
+  }
+
+  speedOptions.selectedIndex = selectedIndex; // eslint-disable-line no-param-reassign
+}
+
+window.onload = function onLoad() {
   const form = document.getElementsByClassName('playback')[0];
 
   form.addEventListener('submit', event => event.preventDefault());
@@ -10,16 +23,3 @@ window.onload = function() {
   // set initial speed
   setSpeed(speedOptions, defaultSpeed);
 };
-
-function setSpeed(speedOptions, speed) {
-  const speedValues = Object.values(speedOptions).map(option =>
-    Number.parseFloat(option.value),
-  );
-
-  let selectedIndex = speedValues.indexOf(speed);
-  if (selectedIndex < 0) {
-    selectedIndex = 3;
-  }
-
-  speedOptions.selectedIndex = selectedIndex;
-}
