fba24a1cebefcdbda4d06bb712c510caa0e9c98a
ibleedfilm <rem@lansangan.com>
2018-06-10 14:38:07 +0800

    Change speed of all videos :sparkles:

--- src/content/index.js
+++ src/content/index.js
@@ -10,7 +10,7 @@ const globalContainer = document.getElementById("globalContainer");
 const throttleAdjustVideoSpeed = throttle(adjustVideoSpeed, 1000, { trailing: true });
 
 const observer = new MutationObserver(mutations => {
-  throttleAdjustVideoSpeed();
+  throttleAdjustVideoSpeed(0.5);
 });
 
 const config = {
@@ -25,5 +25,5 @@ observer.observe(globalContainer, config);
 function adjustVideoSpeed(speed) {
   const videos = document.getElementsByTagName("video");
 
-  Object.values(videos).forEach(video => console.log(video, video.playbackRate));
+  Object.values(videos).forEach(video => video.playbackRate = speed); 
 }
