3b4f82cd7787c39219e21281c58eec188d565086
ibleedfilm <rem@lansangan.com>
2018-06-10 18:46:40 +0800

    Move mutation observer to separate file :truck:

--- src/content/index.js
+++ src/content/index.js
@@ -1,32 +1,4 @@
-import throttle from 'lodash.throttle';
-
-function setVideoSpeed(speed) {
-  const videos = document.getElementsByTagName('video');
-  Object.values(videos).forEach(video => {
-    video.playbackRate = speed; // eslint-disable-line no-param-reassign
-  });
-}
-
-function FacebookObserver(MutationObserver, container, config) {
-  this.MutationObserver = MutationObserver;
-  this.container = container;
-  this.config = config;
-  this.observer = null;
-}
-
-FacebookObserver.prototype.setVideoSpeed = throttle(setVideoSpeed, 1000, {
-  trailing: true,
-});
-
-FacebookObserver.prototype.setObserver = function setObserver(speed) {
-  if (this.observer) {
-    this.observer.disconnect();
-  }
-
-  this.observer = new this.MutationObserver(() => this.setVideoSpeed(speed));
-
-  this.observer.observe(this.container, this.config);
-};
+import FacebookObserver from './observer';
 
 const MutationObserver =
   window.MutationObserver ||
