a4039e5ac55ff375c1659cc162d1094f6ef81055
ibleedfilm <rem@lansangan.com>
2018-06-10 11:06:42 +0800

    Separate webpack environment builds :wrench:

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
 
+const env = process.env.NODE_ENV;
+
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dev'),
+        path: path.resolve(__dirname, env === 'production' ? 'dist' : 'dev'),
     },
+    mode: env === 'production' ? env : 'development',
     module: {
         rules: [
             {
