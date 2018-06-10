a8df0ff783f977c9c74913de6a7f819459bd5719
ibleedfilm <rem@lansangan.com>
2018-06-10 14:48:48 +0800

    Enforce single quotes :rotating_light:

--- src/content/index.js
+++ src/content/index.js
@@ -1,4 +1,4 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
 const MutationObserver =
   window.MutationObserver ||
@@ -6,7 +6,7 @@ const MutationObserver =
   window.MozMutationObserver;
 
 function adjustVideoSpeed(speed) {
-  const videos = document.getElementsByTagName("video");
+  const videos = document.getElementsByTagName('video');
 
   Object.values(videos).forEach(video => {
     video.playbackRate = speed; // eslint-disable-line no-param-reassign
@@ -17,7 +17,7 @@ const throttleAdjustVideoSpeed = throttle(adjustVideoSpeed, 1000, {
   trailing: true,
 });
 
-const globalContainer = document.getElementById("globalContainer");
+const globalContainer = document.getElementById('globalContainer');
 
 const observer = new MutationObserver(() => throttleAdjustVideoSpeed(0.5));
 
