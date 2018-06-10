503668297d9316e76099c681b06348c5959e1a7d
ibleedfilm <rem@lansangan.com>
2018-06-10 19:56:48 +0800

    Initialize app persistent storage :sparkles:

--- src/background/index.js
+++ src/background/index.js
@@ -1,1 +1,11 @@
-console.log('hello background');
+function saveSpeed(speed) {
+  chrome.storage.local.set({ speed }, () => {
+    console.log('storage initialized!');
+  });
+}
+
+chrome.runtime.onInstalled.addListener(() => saveSpeed(1.0));
+
+chrome.storage.local.get(['speed'], result => {
+  console.log(result);
+});
