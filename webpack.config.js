76e5cbe61acd7997a0a827a4834d524647ab1988
ibleedfilm <rem@lansangan.com>
2018-06-10 15:19:20 +0800

    Configure bundling of popup :wrench:

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ const env = process.env.NODE_ENV;
 module.exports = {
     entry: {
         'content-script': './src/content/index.js',
+        'popup': './src/popup/index.js',
     },
     output: {
         path: path.resolve(__dirname, env === 'production' ? 'dist' : 'dev'),
@@ -28,6 +29,10 @@ module.exports = {
     plugins: [
         new CopyWebpackPlugin([
             './src/manifest.json',
+            {
+                from: './src/popup/index.html',
+                to: 'popup.html',
+            },
         ]),
         new CleanWebpackPlugin(
             [env === 'production' ? 'dist' : 'dev'],
