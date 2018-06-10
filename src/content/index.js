45423e0b5a46a459c99e1c88ec310928b15d6d90
ibleedfilm <rem@lansangan.com>
2018-06-10 12:59:59 +0800

    Add Mutation observer :sparkles:

--- src/content/index.js
+++ src/content/index.js
@@ -1,3 +1,19 @@
+const MutationObserver =
+  window.MutationObserver ||
+  window.WebKitMutationObserver ||
+  window.MozMutationObserver;
+
 const globalContainer = document.getElementById("globalContainer");
 
-console.log(globalContainer);
+const observer = new MutationObserver(mutations => {
+  console.log(mutations);
+});
+
+const config = {
+  childList: true,
+  attributes: false,
+  characterData: false,
+  subtree: true
+};
+
+observer.observe(globalContainer, config);
